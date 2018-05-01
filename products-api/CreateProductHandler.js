eb1bd6afe696a1a6141493bf801d36d3ad80ff7a
Asankha C. Perera <asankha@users.noreply.github.com>
2018-05-01 14:51:43 +0530

    initial

--- products-api/CreateProductHandler.js
+++ products-api/CreateProductHandler.js
@@ -1,28 +1,26 @@
 let AWS = require('aws-sdk');
-var uuid = require('uuid');
 const ddb = new AWS.DynamoDB.DocumentClient();
+var uuid = require('uuid');
 exports.handler = function (event, context, callback) {
 
 	console.log('Request to create a product with: ' + JSON.stringify(event));
-	let product = {
-		'id': uuid.v1(),
-		'name': event.name,
-		'price': event.price
-	};
-
-	ddb.put({
+	let product = ddb.put({
 		TableName: 'products_table',
-		Item: product
+		Item: { 
+			'id': uuid.v1(), 
+			'name': event.name, 
+			'price': event.price 
+		},
+		ReturnValues: ALL_NEW
 	}, function (err, data) {
 		if (err) {
 			console.log('Error creating a product' + err);
 			callback(null, 'Error creating product ' + err);
 		} else {
-			console.log('Successfully created a product entry with id : ' +product.id);
+			console.log('Successfully created a product entry with id : ' + product.id);
 			callback(null, JSON.stringify(product));
 		}
 	});
-
 
 	callback(null, 'Successfully executed');
 }
